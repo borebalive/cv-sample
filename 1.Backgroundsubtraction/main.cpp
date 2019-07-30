1de6d4472ef61960b973207f0e78428c8e7085b7
tkh <khtangalive@gmail.com>
2019-07-30 15:39:02 +0800

    2019.7.26

--- 1.Backgroundsubtraction/main.cpp
+++ 1.Backgroundsubtraction/main.cpp
@@ -1,25 +1,87 @@
+/*
+1.KNN
+2.MOG2
+*/
+
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <string>
 
+//#include "background_segm.hpp"
+//#include "bgsegm.hpp"
+
 using namespace std;
 using namespace cv;
 
-
 int main(int argc, const char *argv[])
 {
-	// 输入参数 待整理
-	char *Path;
-	if (argc >= 2) {
-		Path = const_cast<char*>(argv[1]);
-		cout << "run at modify 1" << endl;
+	/// 创建模型
+	Ptr<BackgroundSubtractorMOG2> bg_model = createBackgroundSubtractorMOG2();
+	//createBackgroundSubtractorKNN
+
+	/// 设置模型参数
+	bg_model->setHistory(500);
+	//bg_model->setNMixtures(5);
+	//bg_model->setBackgroundRatio(1);
+	bg_model->setVarThreshold(16.0);
+	//bg_model->setVarThresholdGen
+	//setVarInit // 高斯模型方差初始值 15
+	//setVarMin // 高斯模型方差最小值 4
+	//setVarMax // 高斯模型方差最大值 75
+	//setComplexityReductionThreshold
+	bg_model->setDetectShadows(true);
+	bg_model->setShadowValue(127);
+	//setShadowThreshold
+
+	/// 读取训练视频
+	/*
+	VideoCapture cap;
+	cap.open("1.avi"); // 路径
+
+	if (!cap.isOpened())
+	{
+		cout << "Can not open video file." << endl;
+		return -1;
 	}
-	else {
-		Path = "./data_p_qut2.csv";
-		cout << "run at default" << endl;
+	*/
+	Mat input_frame, output_frame, bgmask;
+
+	string inputName = "D:\\img\\1.jpg";
+	input_frame = imread(inputName);
+
+	//cap >> input_frame;
+
+	if (input_frame.empty())
+	{
+		cout << "Can not read data from the video source." << endl;
+		return -1;
 	}
+
+	/// 训练模型
+	for (int i=0;i<1000;i++)
+	{
+		bg_model->apply(input_frame, bgmask, -1); // 0不更新，1仅根据最新帧更新，-1自动更新率。
+
+		//refineSegments(tmp_frame, bgmask, out_frame);
+	}
+
+	/// 展示模型
+	//getBackgroundImage
+
+	/// 保存模型
+	/*
+	FileStorage fs("bg_model.yml", FileStorage::WRITE);
+	//bg_model->write(fs);
+	fs << "model" << bg_model;
+	*/
+	bg_model->save("bg_model.xml");
+
+	/// 读取模型
+	//bg_model->read();
+	//FileStorage fsRead("bg_model.yml", FileStorage::READ);
+	//Ptr<BackgroundSubtractorMOG2> mog2 = Algorithm::read<BackgroundSubtractorMOG2>(fsRead.root());
 
 	system("pause");
 
-	return 0;
+ 	return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # 平面视觉的项目
 
-1.多边形识别
+测试平台:
+Windows 10
+vs2015
+OpenCV 3.4.2
+
+##0.tools
+读取CSV文件
+ml::TrainData::loadFromCSV
+
+##1.Backgroundsubtraction 背景减除
+
+
+##2.多边形识别
+
